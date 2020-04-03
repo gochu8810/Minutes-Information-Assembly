0406644a304f5c1c1473f8f5d70a20538b7009ef
隼人 奥谷 <okutani@fit-group.jp>
2020-04-03 18:03:30 +0900

    2020/04.03 FIT

---
+++ Minutes Information Assembly/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySQLcomponents;
+
+namespace Minutes_Information_Assembly
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySQLConnect data = new MySQLConnect();
+            //data.SQLstring = "SHOW COLUMNS FROM MinuteTable";
+            data.SQLstring = "SELECT* FROM MinuteTable WHERE MinuteNo=1";
+            List<List<string>> dataList = new List<List<string>>();
+            //List<string> dataList = new List<string>();
+            dataList = data.SelectSQL2(data.SQLstring);
+            
+            MessageBox.Show(string.Join(",", dataList));
+           // data.SQLWrite();
+        }
+    }
+   
+    class TEST
+    {
+        String message;
+        TEST(String message)
+        {
+            this.message = message;
+
+        }
+        void Write()
+        {
+            MessageBox.Show("WriteString");
+        }
+    }
+
+
+
+}
---
+++ Minutes Information Assembly/mySQLClass.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/*
+●Visual StudioとMySQLを接続するプログラムを作成するには
+ソリューションエクスプローラー
+	「参照」を右クリックー「参照の追加...」ー「アセンブリ」－「フレームワーク」を選択
+	
+	「MySql.Data」にチェックを入れて「OK」ボタンを押す
+			…	表示されるフレームワークの数が多すぎるので
+				検索ボックスに「mysql」と入力するとはやい
+*/
+
+// MySQLを使うため
+using MySql.Data.MySqlClient;
+
+namespace MySQLcomponents
+{
+    class MySQLConnect
+    {
+        // MySQLへの接続情報
+        const string server = "35.200.36.4";        // MySQLサーバホスト名
+        const string user = "fit_okutani";          // MySQLユーザ名
+        const string pass = "zips0321";             // MySQLパスワード
+        const string database = "fit_okutani";      // 接続するデータベース名
+        
+
+        public string Sstring{ get; set; }
+
+        /*MySQLデータベースへ接続
+        string connectionString = string.Format("Server={0};Database={1};Uid={2};Pwd={3}", server, database, user, pass);
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        connection.Open();*/
+
+
+        //Select文を代入して、SQLで取得し2次配列のListで返す
+        public List<List<string>> SelectSQL2(string SQLstring)
+        {
+            string connectionString = string.Format("Server={0};Database={1};Uid={2};Pwd={3}", server, database, user, pass);
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            MySqlCommand selectCommand = new MySqlCommand(SQLstring, connection);
+            MySqlDataReader results = selectCommand.ExecuteReader();
+
+            var Datalist2 = new List<List<string>>();
+            Datalist2.Add(new List<string>());
+
+            // 1レコード毎にDatalist(List)に代入
+            while (results.Read()) {
+                int fc = results.FieldCount;
+                List<string> dlist = new List<string>();
+                
+                for (int i = 0; i<=fc-1; i++)
+                {
+                  dlist.Add(results.GetString(i));
+                }
+                Datalist2.Add(dlist);
+            }
+
+            Datalist2.RemoveAt(0);
+            results.Close();
+            connection.Close();
+            return Datalist2;
+        }
+
+        //Select文を代入して、SQLで取得し1次配列のListで返す
+        public List<string> SelectSQL1(string SQLstring)
+        {
+            string connectionString = string.Format("Server={0};Database={1};Uid={2};Pwd={3}", server, database, user, pass);
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            MySqlCommand selectCommand = new MySqlCommand(SQLstring, connection);
+            MySqlDataReader results = selectCommand.ExecuteReader();
+
+            var Datalist1 = new List<string>();
+
+            // 1行ごとにMinutelistに代入
+            while (results.Read()) {
+                Datalist1.Add(results.GetString(0));
+            }
+            results.Close();
+            connection.Close();
+            return Datalist1;
+        }
+
+
+        //MySQLデータベースのInsert, Update,Deleteを行う
+        public void SQLWrite(string SQLstring)
+        {
+            string connectionString = string.Format("Server={0};Database={1};Uid={2};Pwd={3}", server, database, user, pass);
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            // トランザクションを開始します。
+            MySqlTransaction transaction = connection.BeginTransaction();
+            MySqlCommand Writecommand = new MySqlCommand(SQLstring, connection);
+
+            try
+            {
+                Writecommand.Transaction = transaction;
+                Writecommand.ExecuteNonQuery();
+                //トランザクションをコミットします。
+                transaction.Commit();
+            }
+            catch (System.Exception)
+            {
+                //トランザクションをロールバックします。
+                transaction.Rollback();
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+    }
+
+    class SQLstringBuilder : MySQLConnect
+    {
+        public string TableName;
+        public SQLstringBuilder(string TableName)
+        {
+            this.TableName = TableName;
+        }
+
+        string strSELECT()
+        {
+            MySQLConnect strSQL = new MySQLConnect();
+            strSQL.SQLstring = "SELECT * FROM " + TableName;
+            return strSQL.SQLstring;
+        }
+
+        string strWHERE(string ID, string targetID)
+        {
+            MySQLConnect strSQL = new MySQLConnect();
+            strSQL.SQLstring = " WHERE " + ID + "=" + targetID;
+            return strSQL.SQLstring;
+        }
+
+        string strAnd(string ID, string targetID)
+        {
+            MySQLConnect strSQL = new MySQLConnect();
+            strSQL.SQLstring = " AND " + ID + "=" + targetID;
+            return strSQL.SQLstring;
+        }
+
+        string strBETWEEN(string columnName, string targetlow , string targethigh)
+        {
+            MySQLConnect strSQL = new MySQLConnect();
+            strSQL.SQLstring = " BETWEEN " + columnName + "=" + targetlow + " TO " + targethigh;
+            return strSQL.SQLstring;
+        }
+
+        string Writestring()
+        {
+            MySQLConnect strSQL = new MySQLConnect();
+            strSQL.
+            string columns = "";
+            string setvalues = setArray;
+            strSQL.SQLstring = "INSERT INTO " + TableName + "(" + columns + ")" VALUE (" + setvalues + ")";
+            return strSQL.SQLstring;
+        }
+
+
+
+    }
+}
